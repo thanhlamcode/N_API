fa0cccab9379582e309898af74b19e0eec1ba131
thanhlam <lamdoan1122334455@gmail.com>
2025-04-25 09:38:29 +0700

    feat: login

--- api/migrations/Version20250425022154.php
+++ api/migrations/Version20250425022154.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250424164509 extends AbstractMigration
+final class Version20250425022154 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,7 @@ final class Version20250424164509 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            CREATE TABLE "user" (id SERIAL NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, PRIMARY KEY(id))
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON "user" (username)
-        SQL);
+
     }
 
     public function down(Schema $schema): void
@@ -33,9 +28,6 @@ final class Version20250424164509 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             CREATE SCHEMA public
-        SQL);
-        $this->addSql(<<<'SQL'
-            DROP TABLE "user"
         SQL);
     }
 }
