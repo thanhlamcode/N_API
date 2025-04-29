2ecfbad4d7090c266aafb7e0494bc57e74aa8c60
thanhlam <lamdoan1122334455@gmail.com>
2025-04-29 11:32:58 +0700

    chore: IRI for post operations

---
+++ api/src/Serializer/EntityIdToIriDenormalizer.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Serializer;
+
+use ApiPlatform\Metadata\IriConverterInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractorInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class EntityIdToIriDenormalizer implements DenormalizerInterface
+{
+    public function __construct(
+        #[Autowire(service: 'api_platform.iri_converter')]
+        private IriConverterInterface $iriConverter,
+
+        #[Autowire(service: 'api_platform.serializer.normalizer.item')]
+        private NormalizerInterface $normalizer,
+
+        #[Autowire(service: 'property_info')]
+        private PropertyInfoExtractorInterface $propertyInfoExtractor,
+
+        private EntityManagerInterface $entityManager, // THÊM EntityManager
+    ) {}
+
+    public function supportsDenormalization($data, $type, $format = null, array $context = []): bool
+    {
+        return class_exists($type) && str_starts_with($type, 'App\\Entity\\');
+    }
+
+    public function denormalize($data, $type, $format = null, array $context = []): mixed
+    {
+        if (!is_array($data)) {
+            return $this->normalizer->denormalize($data, $type, $format, $context);
+        }
+
+        foreach ($data as $property => $value) {
+            if (is_numeric($value)) {
+                $types = $this->propertyInfoExtractor->getTypes($type, $property);
+
+                if ($types !== null) {
+                    foreach ($types as $typeInfo) {
+                        if (
+                            $typeInfo->getBuiltinType() === 'object' &&
+                            $typeInfo->getClassName() !== null &&
+                            str_starts_with($typeInfo->getClassName(), 'App\\Entity\\')
+                        ) {
+                            $resourceClass = $typeInfo->getClassName();
+                            $entity = $this->entityManager->find($resourceClass, (int)$value);
+
+                            if ($entity) {
+                                $data[$property] = $this->iriConverter->getIriFromResource($entity);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return $this->normalizer->denormalize($data, $type, $format, $context);
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            '*' => false,
+        ];
+    }
+}
